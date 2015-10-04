38044c46847e2d73e925eae15d7236f21990b85b
Tyan Vyacheslav <chapaevez@yabdex.ru>
2015-10-04 19:53:49 +0400

    tasks 3-7

--- arrays.cpp
+++ arrays.cpp
@@ -39,7 +39,7 @@ void task_4()
 {
     // Task 4: asserts at the end must hold
     // Hint: fix with initialization (not assignment)
-    int a[5];
+	int a[5]{1, 2, 3, 4, 5};;
     
     assert(a[0] == 1);
     assert(a[1] == 2);
@@ -52,28 +52,30 @@ void task_4()
 // copy arr1 to arr2
 void task_5_copy(int * arr1, int * arr2, int size)
 {
-    arr2 = arr1;
+	for (int i = 0; i < size; ++i)
+	{
+		arr2[i] = arr1[i];
+	}
 }
 
 // Task 6
 // copy array `arr1` to array `arr2` of the same size
-void task_6_poor_copy(int * arr1, int * arr2)
+void task_6_poor_copy(int * arr1, int * arr2, int size)
 {
-    // Hint: something wrong here; test this from main()
-    for(int i = 0; i < sizeof(arr2); ++i)
-    {
-        arr2[i] = arr1[i];
-    }
+	for (int i = 0; i < size; ++i)
+	{
+		arr2[i] = arr1[i];
+	}
 }
 
 // Task 7
 // print array `arr`
 void task_7_print(int * arr, int size, char delim = ' ')
 {
-    // Hint: something wrong here; run this from main()
-    for(int i = 0; i <= size; ++i)
+ 
+    for(int i = 0; i < size; ++i)
     {
-     //   std::cout << a[i] << delim;
+     std::cout << arr[i] << delim;
     }
 }
 
