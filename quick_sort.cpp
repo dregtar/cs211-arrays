3bc323467f35960d458e418dcab087cd3fa75ac4
Tyan Vyacheslav <chapaevez@yabdex.ru>
2015-10-08 21:10:11 +0400

    Исправлен неправильный 2-й параметр в вызове partition, изменен сам partition - передается указатель на pivot, перед началом перемещаем pivot в конец, после ставим на место, указываемое store

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
  
 #include <cstdlib>
- 
+using namespace std;
 using std::rand;
 using std::swap;
  
@@ -10,19 +10,28 @@ using std::swap;
 // заданной отрезком [left, right), так что в начале
 // следуют элементы меньшие pivot, а в конце - большие;
 // возвращает место начала блока элементов, больших pivot;
-int * partition(int * left, int *  right, int pivot) {
+int * partition(int * left, int *  right, int * pivot) {
 	int * store = left; // место для вставки элементов, меньших pivot
+	int inPivot = *pivot;
+	swap(*pivot, *right);
 	for (int * p = left; p != right; ++p)
-		if (*p < pivot)
+		if (*p < inPivot)
 			swap(*p, *store++);
+	swap(*store, *right);
 	return store;
 }
  
 void my_qsort(int * arr, int n) {
 	if (n <= 1)
 		return; // массив в 1 или 0 элементов уже упорядочен
+	
 	int * pivotPtr = arr + rand() % n; // случайный выбор опорного элемента
-	int newPivotIdx = partition(arr, arr + n, *pivotPtr) - arr;
-	my_qsort(arr, newPivotIdx + 1);
-	my_qsort(arr + newPivotIdx, n - (newPivotIdx + 1));
+	int newPivotIdx = partition(arr, arr + n-1, pivotPtr) - arr;
+	my_qsort(arr, newPivotIdx );
+	my_qsort(arr + newPivotIdx + 1, n - (newPivotIdx + 1));
+}
+
+int main(){
+	int a[6]{3,2,1,4,5,66};
+	my_qsort(a,6);
 }
